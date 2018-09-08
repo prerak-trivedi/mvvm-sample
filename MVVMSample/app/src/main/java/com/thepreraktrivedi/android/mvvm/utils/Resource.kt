0e22e60836e1064ecdb84ddc24484c0225fd1ae0
Prerak Trivedi <prerak.trivedi@postmates.com>
2018-09-08 13:44:41 -0700

    Update to latest dependencies

--- MVVMSample/app/src/main/java/com/thepreraktrivedi/android/mvvm/utils/Resource.kt
+++ MVVMSample/app/src/main/java/com/thepreraktrivedi/android/mvvm/utils/Resource.kt
@@ -1,5 +1,11 @@
 package com.thepreraktrivedi.android.mvvm.utils
 
+/**
+ * Inspired by https://developer.android.com/topic/libraries/architecture/guide.html#addendum.
+ *
+ * This represents a simple resource from the network that our services will return
+ * to indicate success or failure.
+ */
 class Resource<T> private constructor(val status: Status,
                                       val data: T?,
                                       val error: Exception?) {
